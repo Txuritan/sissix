fdefc3084c7b10c1f704a303dbbb31b6a79dd495
Txuritan <txuritan@protonmail.com>
2024-05-16 15:15:50 -0400

    add html description and maskable icons

--- vite.config.ts
+++ vite.config.ts
@@ -106,7 +106,55 @@ export default defineConfig({
                         "src": "android-chrome-512x512.png",
                         "sizes": "512x512",
                         "type": "image/png"
-                    }
+                    },
+                    {
+                        "src": "maskable-icon-48x48.png",
+                        "sizes": "48x48",
+                        "type": "image/png",
+                        "purpose": "maskable"
+                    },
+                    {
+                        "src": "maskable-icon-72x72.png",
+                        "sizes": "72x72",
+                        "type": "image/png",
+                        "purpose": "maskable"
+                    },
+                    {
+                        "src": "maskable-icon-96x96.png",
+                        "sizes": "96x96",
+                        "type": "image/png",
+                        "purpose": "maskable"
+                    },
+                    {
+                        "src": "maskable-icon-144x144.png",
+                        "sizes": "144x144",
+                        "type": "image/png",
+                        "purpose": "maskable"
+                    },
+                    {
+                        "src": "maskable-icon-192x192.png",
+                        "sizes": "192x192",
+                        "type": "image/png",
+                        "purpose": "maskable"
+                    },
+                    {
+                        "src": "maskable-icon-384x384.png",
+                        "sizes": "384x384",
+                        "type": "image/png",
+                        "purpose": "maskable"
+                    },
+                    {
+                        "src": "maskable-icon-512x512.png",
+                        "sizes": "512x512",
+                        "type": "image/png",
+                        "purpose": "maskable"
+                    },
+                    {
+                        "src": "maskable-icon-1024x1024.png",
+                        "sizes": "1024x1024",
+                        "type": "image/png",
+                        "purpose": "maskable"
+                    },
                 ]
             },
         }),
