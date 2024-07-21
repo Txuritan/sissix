cd9ed08eb721c9d38f988cae7c8d3e2172f58246
Txuritan <txuritan@protonmail.com>
2024-07-21 16:34:55 -0400

    actually use postcss instead of just tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
-module.exports = {
+export default {
     plugins: {
         'postcss-import': {},
         tailwindcss: {},
         autoprefixer: {},
+        ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
     }
 }
